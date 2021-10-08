059f5e80ac5d1748dc0fff111318395b4d14715d
abheekt <abheekt@gmail.com>
2021-10-08 12:49:40 +0100

    Changes to the test file by Abheek

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,6 +34,14 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World, Thanks for teaching me DevOps and Maven";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
